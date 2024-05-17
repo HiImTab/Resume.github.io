13e59a59787f055fc34263e7d411a59b1c64d02c
Tabitha Troup <54299538+HiImTab@users.noreply.github.com>
2024-05-16 17:19:49 -0700

    Updated app.js, index.html, and style.css
    Updated text on index.html
    Added button to change colors on light/dark mode
    Added white hover effect on dark mode

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector('button');
+const body = document.querySelector('body');
 let count = 0;
+body.classList.add('light-mode');
 
 btn.addEventListener('click', function(){
     
@@ -9,6 +11,12 @@ btn.addEventListener('click', function(){
         const darkModeText = document.body.style.color = 'white';
         document.body.style.backgroundColor = darkModeBackground;
         document.body.style.color = darkModeText;
+        btn.style.backgroundColor = 'rgb(34, 34, 34)';
+        btn.style.color = 'white';
+        btn.style.border = '1px solid white';
+
+        body.classList.add('dark-mode');
+        body.classList.remove('light-mode');
 
         document.querySelector('a').style.color = 'white';
         let links = document.getElementsByClassName('link');
@@ -23,6 +31,12 @@ btn.addEventListener('click', function(){
         const lightModeText = document.body.style.color = 'black';
         document.body.style.backgroundColor = lightModeBackground;
         document.body.style.color = lightModeText;
+        btn.style.backgroundColor = 'rgb(255, 255, 255)';
+        btn.style.color = 'black';
+        btn.style.border = '1px solid black';
+
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
 
         document.querySelector('a').style.color = 'black';
         let links = document.getElementsByClassName('link');
